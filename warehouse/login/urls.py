3fa8d7d7b5912802a96c66786acee1d10d1893a2
stefvanhouten <stefvanhouten@gmail.com>
2022-06-01 12:51:50 +0200

    Update urls.py

--- warehouse/login/urls.py
+++ warehouse/login/urls.py
@@ -2,10 +2,8 @@ from warehouse.login import login
 
 urls = [
     ("/login", (login.PageMaker, "HandleLogin"), "POST"),
-    ("/login", (login.PageMaker, "RequestLogin")),
     ("/logout", (login.PageMaker, "RequestLogout")),
     ("/usersettings", (login.PageMaker, "RequestUserSettings")),
-    ("/apisettings", (login.PageMaker, "RequestApiSettings")),
     ("/resetpassword", (login.PageMaker, "RequestResetPassword")),
     ("/resetpassword/([^/]*)/(.*)", (login.PageMaker, "RequestResetPassword")),
 ]
