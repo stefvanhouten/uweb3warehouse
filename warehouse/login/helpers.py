b856346d13bf2b0fba186c7006c74fb71ee4b542
stefvanhouten <stefvanhouten@gmail.com>
2022-06-04 13:12:46 +0200

    Add ApiUserService. Split LoginService logic into smaller chunks

--- warehouse/login/helpers.py
+++ warehouse/login/helpers.py
@@ -5,28 +5,18 @@ from warehouse.login import model
 
 class LoginService:
     def __init__(self, connection):
-        self._auth_failed = False
         self._user = None
+        self._auth_failed = False
         self.connection = connection
+        self.session = model.Session(self.connection)
 
     def authenticate(self):
         if self._auth_failed:
             raise ValueError("Authentication failed")
 
         if not self._user and not self._auth_failed:
-            session = model.Session(self.connection)
-
-            try:
-                userID = int(str(session.rawcookie))
-            except Exception as ex:
-                self._auth_failed = True
-                session.Delete()
-                raise ValueError("Session cookie invalid") from ex
-
-            try:
-                self._user = model.User.FromPrimary(self.connection, userID)
-            except uweb3.model.NotExistError:
-                self._auth_failed = True
+            userID = self._get_userid_from_cookie()
+            self._user = self._get_user(userID)
 
         if not self._user:
             raise ValueError("User not valid")
@@ -34,6 +24,25 @@ class LoginService:
         if self._user["active"] != "true":
             raise ValueError("User not active, session invalid")
         return self._user
+
+    def _get_userid_from_cookie(self):
+        try:
+            userID = int(str(self.session.rawcookie))
+        except Exception as ex:
+            self._fail_authentication()
+            raise ValueError("Session cookie invalid") from ex
+        return userID
+
+    def _get_user(self, userID):
+        try:
+            return model.User.FromPrimary(self.connection, userID)
+        except uweb3.model.NotExistError:
+            self._fail_authentication()
+        return None
+
+    def _fail_authentication(self):
+        self._auth_failed = True
+        self.session.Delete()
 
 
 class LoginServiceBuilder:
@@ -43,6 +52,34 @@ class LoginServiceBuilder:
     def __call__(self, connection, **ignored):
         if not self._instance:
             self._instance = LoginService(connection)
+        return self._instance
+
+
+class ApiUserService:
+    def __init__(self, connection, apikey):
+        self.apikey = apikey
+        self._instance = None
+        self.connection = connection
+
+    def authenticate(self):
+        if not self._instance:
+            self._instance = self._get_user()
+        return self._instance
+
+    def _get_user(self):
+        try:
+            return model.Apiuser.FromKey(self.connection, self.apikey)
+        except uweb3.model.NotExistError as ex:
+            raise ValueError("The given API key is not valid") from ex
+
+
+class ApiUserServiceBuilder:
+    def __init__(self):
+        self._instance = None
+
+    def __call__(self, connection, apikey, **ignored):
+        if not self._instance:
+            self._instance = ApiUserService(connection, apikey)
         return self._instance
 
 
